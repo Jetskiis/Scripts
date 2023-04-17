98ac72c64dc8bdaa587993a0470fa5e2e4aff5a5
Jetskiis <yegene60@gmail.com>
2023-04-17 11:43:57 -0400

    add reddit feed scraper script

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Scripts
 Basic Python scripts used for web scraping, automation, and whatever else
 
+reddit_feed.py - A script that logins to a reddit user account via Praw and returns the top 25 posts in the "best" and "hot" category. It then sends an email with the posts in table format using pandas.
--- reddit_feed.py
+++ reddit_feed.py
@@ -1,26 +1,73 @@
 import os
+import smtplib
+from email.message import EmailMessage
 
 import pandas as pd
+import praw
 import requests
 from dotenv import load_dotenv
+from IPython.display import HTML
 
 load_dotenv()
-# oauth
+# set up oauth and connect to Reddit API via PRAW
 client_id, client_key = os.getenv("CLIENT_ID"), os.getenv("SECRET_KEY")
 auth = requests.auth.HTTPBasicAuth(client_id, client_key)
 
-data = {
-    'grant_type': 'password',
-    'username': os.getenv("USERNAME"),
-    'password': os.getenv("PASSWORD"),
-}
+reddit = praw.Reddit(client_id=client_id, client_secret=client_key, username=os.getenv(
+    "REDDIT_USERNAME"), password=os.getenv("REDDIT_PASSWORD"), user_agent="MyBot/0.0.1 by @Jetskiis")
 
-headers = {'User-Agent': 'MyBot/0.0.1'}
+# initialize Pandas DataFrame
 
-res = requests.post("https://www.reddit.com/api/v1/access_token",
-                    auth=auth, data=data, headers=headers)
+hot_df = pd.DataFrame()
+best_df = pd.DataFrame()
 
-token = res.json()['access_token']
-headers["Authorization"] = f"bearer {token}"
-res = requests.get("https://oauth.reddit.com/api/v1/me",
-                   headers=headers).json()
+hot_df.style.set_table_styles(
+    [dict(selector='th', props=[('text-align', 'left')])])
+best_df.style.set_table_styles(
+    [dict(selector='th', props=[('text-align', 'left')])])
+
+
+
+# build dataframe
+# subscribed_subreddits = reddit.user.subreddits(limit=None)
+
+for post in reddit.front.best(limit=25):
+    new_row = pd.DataFrame(
+        {"Title": "-  " + post.title, "Subreddit": f'<a href="https://new.reddit.com{post.permalink}">{post.subreddit}</a>', "Score": post.score}, index=pd.Index([len(best_df)]))
+    best_df = pd.concat([best_df, new_row])
+
+
+for post in reddit.front.hot(limit=25):
+    new_row = pd.DataFrame(
+        {"Title": "-  " + post.title, "Subreddit": f'<a href="https://new.reddit.com{post.permalink}">{post.subreddit}</a>', "Score": post.score}, index=pd.Index([len(hot_df)]))
+    hot_df = pd.concat([hot_df, new_row])
+
+
+# print(best_df)
+# print(hot_df)
+
+# build output html
+html = """\
+<html>
+  <head></head>
+  <body style="font-size: 1rem">
+  <h2 style="text-align: center">Sort by Best</h2>
+    {0}
+  <h2 style="text-align: center">Sort by Hot</h2>
+    {1}
+  </body>
+</html>
+""".format((best_df.to_html(escape=False, index=False, justify="center", border=0)), (hot_df.to_html(escape=False, index=False, justify="center", border=0)))
+
+# send data to email
+msg = EmailMessage()
+msg["Subject"] = "Reddit Feed"
+msg["From"] = os.getenv("EMAIL")
+msg["To"] = os.getenv("RECIPIENT_EMAIL")
+msg.set_content(html, subtype="html")
+
+s = smtplib.SMTP('smtp-mail.outlook.com', 587)
+s.starttls()
+s.login(os.getenv("EMAIL"), os.getenv("EMAIL_PASSWORD"))
+s.send_message(msg)
+s.quit()
